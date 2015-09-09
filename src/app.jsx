b5f2030d859907d2f71aea484c97f3e45b5a6884
peterhwang2010 <inhohwang2010@gmail.com>
2015-09-09 14:10:43 -0400

    added routes to src

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,27 @@
 var React = require('react');
+var Routes = require('./routes');
 
 var Hello = React.createClass({
   render: function() {
     return <h1 className="red">
-      Hello!
+      {this.props.children}
     </h1>
   }
 });
 
-var element = React.createElement(Hello, {});
-React.render(element, document.querySelector('.container'));
+var Child1 = React.createClass({
+	render: function(){
+		return <h1>Im child1</h1>
+		{this.props.children}
+	}
+});
+
+var Child2 = React.createClass({
+	render: function(){
+		return <h1> Im the other child </h1> 
+	}
+});
+
+
+// var element = React.createElement(Hello, {});
+React.render(Routes, document.querySelector('.container'));
