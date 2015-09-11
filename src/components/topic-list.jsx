41cdcdbca8154a2c7ea5df3b5bf606ca48cae241
peterhwang2010 <inhohwang2010@gmail.com>
2015-09-11 00:36:11 -0400

    added topics route in the router

--- src/components/topic-list.jsx
+++ src/components/topic-list.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var Reflux = require('reflux');
 var TopicStore = require('../stores/topic-store');
 var Actions = require('../actions');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 module.exports = React.createClass({
 	mixins: [
@@ -22,10 +24,10 @@ module.exports = React.createClass({
 	},
 	renderTopics: function(){
 		return this.state.topics.map(function(topic){
-			return <li className="list-group-item" key={topic.id}>
+			return <Link to={"topics/" + topic.id}className="list-group-item" key={topic.id}>
 				<h4>{topic.name}</h4> 
 				<p>{topic.description}</p>
-			</li>
+			</Link>
 		});
 	},
 	onChange: function(event, topics){
