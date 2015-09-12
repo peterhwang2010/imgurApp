d05df3688a74444854bf506d59476a81d408ff4d
peterhwang2010 <inhohwang2010@gmail.com>
2015-09-12 02:58:26 -0400

    retrieved images from api

---
+++ src/components/image-preview.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+module.exports = React.createClass({
+	render: function(){
+		return <div>
+			I am a image with id {this.props.id}
+			</div>
+	}
+});
--- src/stores/image-store.jsx
+++ src/stores/image-store.jsx
@@ -3,6 +3,7 @@ var Api = require('../utils/api');
 var Actions = require('../actions');
 
 module.exports = Reflux.createStore({
+	listenables: [Actions],
 	getImages: function(topicId){
 		Api.get('topics/' + topicId)
 			.then(function(json){
